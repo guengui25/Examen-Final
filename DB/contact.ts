9e7922957507d0b2e461cfdcc5e26a3c92e927c0
guengui25 <guengui25@hotmail.com>
2024-01-19 17:36:18 +0100

    updt

--- DB/contact.ts
+++ DB/contact.ts
@@ -1,15 +1,13 @@
 import mongoose from "mongoose"
 
 import { Contact } from "../types.ts"
-import { GraphQLError } from "graphql";
 
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-
     //_id de mongoose
     nombre:{type:String,required:true},
-    telefono:{type:String,required:true,unique:true},
+    telefono:{type:String,required:true,unique:true}, // Con el unique me aseguro de que no se repita
     pais:{type:String,required:true},
 })
 
